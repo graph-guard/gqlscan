668ee50eee2b26cdbedcc96164332c6225ad3ece
Roman Sharkov <roman.scharkov@gmail.com>
2022-05-24 15:57:50 +0200

    docs: Add package documentation

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Package gqlscan provides functions for fast and allocation-free
+// lexical scanning and validation of GraphQL queries according
+// to the GraphQL specification of October 2021
+// (https://spec.graphql.org/October2021/).
+//
+// The provided functions don't perform semantic analysis such as
+// making sure that declared variables are used or that
+// values match their declared types, etc. as this is outside the scope
+// of lexical analysis.
+package gqlscan
